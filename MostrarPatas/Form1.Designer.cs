b726ecd90ffe46a2c43808ebc8498ba7cc822a80
ValterSilvaJr <valtersilvajr@live.com>
2020-04-08 20:53:22 -0300

    adicionado o exercicios MostrarPatas, utilização de ComboBox e PictureBox com Switch.

---
+++ MostrarPatas/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace MostrarPatas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboAnimais = new System.Windows.Forms.ComboBox();
+            this.btnPatas = new System.Windows.Forms.Button();
+            this.pctAnimais = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctAnimais)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboAnimais
+            // 
+            this.cboAnimais.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboAnimais.FormattingEnabled = true;
+            this.cboAnimais.Location = new System.Drawing.Point(12, 30);
+            this.cboAnimais.Name = "cboAnimais";
+            this.cboAnimais.Size = new System.Drawing.Size(345, 21);
+            this.cboAnimais.Sorted = true;
+            this.cboAnimais.TabIndex = 0;
+            // 
+            // btnPatas
+            // 
+            this.btnPatas.Location = new System.Drawing.Point(67, 57);
+            this.btnPatas.Name = "btnPatas";
+            this.btnPatas.Size = new System.Drawing.Size(219, 23);
+            this.btnPatas.TabIndex = 1;
+            this.btnPatas.Text = "Mostrar Patas";
+            this.btnPatas.UseVisualStyleBackColor = true;
+            this.btnPatas.Click += new System.EventHandler(this.btnPatas_Click);
+            // 
+            // pctAnimais
+            // 
+            this.pctAnimais.Location = new System.Drawing.Point(12, 104);
+            this.pctAnimais.Name = "pctAnimais";
+            this.pctAnimais.Size = new System.Drawing.Size(345, 187);
+            this.pctAnimais.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctAnimais.TabIndex = 2;
+            this.pctAnimais.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 303);
+            this.Controls.Add(this.pctAnimais);
+            this.Controls.Add(this.btnPatas);
+            this.Controls.Add(this.cboAnimais);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Animais";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctAnimais)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboAnimais;
+        private System.Windows.Forms.Button btnPatas;
+        private System.Windows.Forms.PictureBox pctAnimais;
+    }
+}
+
---
+++ MostrarPatas/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MostrarPatas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Limpar os itens adicionados no ComboBox
+            cboAnimais.Items.Clear();
+
+            //Adiciona itens ao ComboBox por meio do método Add()
+            cboAnimais.Items.Add("Cachorro");
+            cboAnimais.Items.Add("Cavalo");
+            cboAnimais.Items.Add("Gato");
+            cboAnimais.Items.Add("Centopeia");
+            cboAnimais.Items.Add("Cobra");
+            cboAnimais.Items.Add("Pássaro");
+        }
+
+        private void btnPatas_Click(object sender, EventArgs e)
+        {
+            //Declaração das variáveis
+            string strAnimal;
+
+            //A variável strAnimal irá receber o valor atual do ComboBox
+            strAnimal = cboAnimais.Text;
+
+            //Instrução de seleção
+            switch (strAnimal)
+            {
+                case "Cavalo":
+                    pctAnimais.Load("cavalo.jpg");
+                    MessageBox.Show("Este Animal tem 4 patas", "Animais", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                case "Gato":
+                    pctAnimais.Load("gato.jpg");
+                    MessageBox.Show("Este Animal tem 4 patas", "Animais", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                case "Cachorro":
+                    pctAnimais.Load("cachorro.jpg");
+                    MessageBox.Show("Este Animal tem 4 patas", "Animais", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+
+                case "Centopeia":
+                    pctAnimais.Load("centopeia.jpg");
+                    MessageBox.Show("Este Animal tem 100 patas", "Animais", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+
+                case "Cobra":
+                    pctAnimais.Load("cobra.jpg");
+                    MessageBox.Show("Este Animal não tem patas", "Animais", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+
+                case "Pássaro":
+                    pctAnimais.Load("passaro.jpg");
+                    MessageBox.Show("Este Animal tem 2 patas", "Animais", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+
+                default:
+                    pctAnimais.ResetText();
+                    MessageBox.Show("Selecione um Animal", "Animais", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
+        }
+    }
+}
