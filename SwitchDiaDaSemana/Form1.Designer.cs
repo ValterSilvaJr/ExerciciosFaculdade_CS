09e6318314bc438a03481f410882442a333de2ee
ValterSilvaJr <valtersilvajr@live.com>
2020-04-08 18:39:07 -0300

    Adicionado a cláusula switch

---
+++ SwitchDiaDaSemana/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace SwitchDiaDaSemana
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDia = new System.Windows.Forms.Label();
+            this.lblDiaSemana = new System.Windows.Forms.Label();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblDia
+            // 
+            this.lblDia.AutoSize = true;
+            this.lblDia.Location = new System.Drawing.Point(24, 9);
+            this.lblDia.Name = "lblDia";
+            this.lblDia.Size = new System.Drawing.Size(23, 13);
+            this.lblDia.TabIndex = 0;
+            this.lblDia.Text = "Dia";
+            // 
+            // lblDiaSemana
+            // 
+            this.lblDiaSemana.AutoSize = true;
+            this.lblDiaSemana.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiaSemana.ForeColor = System.Drawing.Color.Red;
+            this.lblDiaSemana.Location = new System.Drawing.Point(70, 51);
+            this.lblDiaSemana.Name = "lblDiaSemana";
+            this.lblDiaSemana.Size = new System.Drawing.Size(57, 20);
+            this.lblDiaSemana.TabIndex = 1;
+            this.lblDiaSemana.Text = "label2";
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(27, 96);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(75, 23);
+            this.btnVerificar.TabIndex = 2;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(117, 96);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 3;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // txtDia
+            // 
+            this.txtDia.Location = new System.Drawing.Point(74, 6);
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(118, 20);
+            this.txtDia.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(215, 170);
+            this.ControlBox = false;
+            this.Controls.Add(this.txtDia);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.lblDiaSemana);
+            this.Controls.Add(this.lblDia);
+            this.Name = "Form1";
+            this.Text = "Selecionar Dia";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDia;
+        private System.Windows.Forms.Label lblDiaSemana;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.TextBox txtDia;
+    }
+}
+
