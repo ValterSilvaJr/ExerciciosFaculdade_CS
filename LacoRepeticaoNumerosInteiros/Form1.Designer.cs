42239e8234f7a6b7ea7cd1aab4608d1c8396c504
ValterSilvaJr <valtersilvajr@live.com>
2020-04-09 19:51:39 -0300

    adição de exercicios de estrutura de repetição for, while e do while

---
+++ LacoRepeticaoNumerosInteiros/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace LacoRepeticaoNumerosInteiros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstNumeros = new System.Windows.Forms.ListBox();
+            this.btnFor = new System.Windows.Forms.Button();
+            this.btnWhile = new System.Windows.Forms.Button();
+            this.btnDo = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstNumeros
+            // 
+            this.lstNumeros.FormattingEnabled = true;
+            this.lstNumeros.Location = new System.Drawing.Point(12, 12);
+            this.lstNumeros.Name = "lstNumeros";
+            this.lstNumeros.Size = new System.Drawing.Size(132, 329);
+            this.lstNumeros.TabIndex = 0;
+            // 
+            // btnFor
+            // 
+            this.btnFor.Location = new System.Drawing.Point(167, 53);
+            this.btnFor.Name = "btnFor";
+            this.btnFor.Size = new System.Drawing.Size(87, 23);
+            this.btnFor.TabIndex = 1;
+            this.btnFor.Text = "Laço For";
+            this.btnFor.UseVisualStyleBackColor = true;
+            this.btnFor.Click += new System.EventHandler(this.btnFor_Click);
+            // 
+            // btnWhile
+            // 
+            this.btnWhile.Location = new System.Drawing.Point(167, 113);
+            this.btnWhile.Name = "btnWhile";
+            this.btnWhile.Size = new System.Drawing.Size(87, 23);
+            this.btnWhile.TabIndex = 2;
+            this.btnWhile.Text = "Laço While";
+            this.btnWhile.UseVisualStyleBackColor = true;
+            this.btnWhile.Click += new System.EventHandler(this.btnWhile_Click);
+            // 
+            // btnDo
+            // 
+            this.btnDo.Location = new System.Drawing.Point(167, 173);
+            this.btnDo.Name = "btnDo";
+            this.btnDo.Size = new System.Drawing.Size(87, 23);
+            this.btnDo.TabIndex = 3;
+            this.btnDo.Text = "Laço Do While";
+            this.btnDo.UseVisualStyleBackColor = true;
+            this.btnDo.Click += new System.EventHandler(this.btnDo_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(167, 233);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(87, 23);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 354);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnDo);
+            this.Controls.Add(this.btnWhile);
+            this.Controls.Add(this.btnFor);
+            this.Controls.Add(this.lstNumeros);
+            this.Name = "Form1";
+            this.Text = "Números Inteiros";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstNumeros;
+        private System.Windows.Forms.Button btnFor;
+        private System.Windows.Forms.Button btnWhile;
+        private System.Windows.Forms.Button btnDo;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ LacoRepeticaoNumerosInteiros/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LacoRepeticaoNumerosInteiros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFor_Click(object sender, EventArgs e)
+        {
+            //Limpa a ListBox
+            lstNumeros.Items.Clear();
+
+            //Declaração da Variável controladora do laço
+            int i;
+
+            for(i = 0; i <= 100; i++)
+            {
+                //Acrescenta o valor da variável i a ListBox
+                lstNumeros.Items.Add(i);
+            }
+        }
+
+        private void btnWhile_Click(object sender, EventArgs e)
+        {
+            //Limpar a ListBox
+            lstNumeros.Items.Clear();
+
+            //Declaração da variável controladora do laço
+            int i = 1;
+
+            while(i <= 100)
+            {
+                //Acrescenta o valor da variável i a ListBox
+                lstNumeros.Items.Add(i);
+
+                //Incrementa a variável controladora
+                i++;
+            }
+        }
+
+        private void btnDo_Click(object sender, EventArgs e)
+        {
+            //Limpa a ListBox
+            lstNumeros.Items.Clear();
+
+            //Declaração da variável controladora do laço
+            int i = 1;
+
+            do
+            {
+                //Acrescenta o valor da variável i a ListBox
+                lstNumeros.Items.Add(i);
+
+                //Incrementa a variável controladora
+                i++;
+
+            } while (i <= 100);
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            //Limpa a ListBox
+            lstNumeros.Items.Clear();
+        }
+    }
+}
