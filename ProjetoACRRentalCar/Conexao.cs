9468dc61c7abce2d8902abab20cb30e2169aa9f6
ValterSilvaJr <valtersilvajr@live.com>
2020-04-15 23:15:21 -0300

    criação do formulário de cadastro de clientes

---
+++ ProjetoACRRentalCar/Conexao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Definição dos namespaces usados no projeto para o SQL SERVER
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ProjetoACRRentalCar
+{
+    public class Conexao
+    {
+        //Método para criar a conexão com o banco de dados
+        public static SqlConnection getConnection()
+        {
+            //Cria um objeto (variável cnn) do tipo SqlConnection e configura a String de conexão
+            SqlConnection cnn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;
+                                                    Initial Catalog=DB_RENTALCAR;
+                                                    Integrated Security=True;
+                                                    Pooling=False");
+
+            //Retorna a conexão com o banco dados quando este método for chamado
+            return cnn;
+        }
+
+    }
+}
